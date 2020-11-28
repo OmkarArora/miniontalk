3140c9c7424b6b51f33b66fd40e8fa944de47f81
Omkar Arora <aroraomkar@gmail.com>
2020-11-28 15:26:16 +0530

    feat: translation api connected

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
 function clickEventHandler() {
-    // console.log(textInput.value);
-    outputDiv.innerText = "translated: " + textInput.value;
+  let inputText = textInput.value;
+
+  //call server for processing
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      outputDiv.innerText = json.contents.translated;
+    })
+    .catch((error) => console.log("error occurred", error));
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);

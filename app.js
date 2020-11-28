869cb583a65c53d31a358e3c4bd17ac89f980e88
Omkar Arora <aroraomkar@gmail.com>
2020-11-28 13:52:23 +0530

    feat: I/O added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
 
 function clickEventHandler() {
-    console.log(textInput.value);
-    console.log("click");
+    // console.log(textInput.value);
+    outputDiv.innerText = "translated: " + textInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler);

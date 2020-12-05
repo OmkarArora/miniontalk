2d216c95154b0499130e756a88f4b6e05444965f
Omkar Arora <aroraomkar@gmail.com>
2020-12-05 21:36:35 +0530

    feat: translation API

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var outputDiv = document.querySelector("#output");
 var loader = document.querySelector(".loader");
 var translatedSection = document.querySelector(".translated-section");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-// var serverURL = "https://api.funtranslations.com/translate/minion.json";
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text) {
   return serverURL + "?" + "text=" + text;

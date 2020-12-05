5ce5bca0f4ad5a998d021aea247c9c09441393d8
Omkar Arora <aroraomkar@gmail.com>
2020-12-05 21:34:34 +0530

    refactor, feat: loader, gif

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
+var loader = document.querySelector(".loader");
+var translatedSection = document.querySelector(".translated-section");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+// var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function getTranslationURL(text) {
   return serverURL + "?" + "text=" + text;
@@ -11,11 +13,22 @@ function getTranslationURL(text) {
 
 function clickEventHandler() {
   let inputText = textInput.value;
+  
+// Loader set to ON
+loader.style.display = "block";
+
+// Hide output section
+translatedSection.style.display = "none";
 
   //call server for processing
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
     .then((json) => {
+      // Turn loader off
+      loader.style.display = "none";
+
+      // Translated section now visible
+      translatedSection.style.display = "unset";
       outputDiv.innerText = json.contents.translated;
     })
     .catch((error) => console.log("error occurred", error));
